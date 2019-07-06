2a2e039d4423e8fc8cd655c52f858cf2b39d32ca
Zhao TengHu <z.th.btc@gmail.com>
2019-07-06 10:52:40 -0700

    Remove console log

--- test/expertise_update.test.js
+++ test/expertise_update.test.js
@@ -1,6 +1,7 @@
 import Promise from 'bluebird';
 import should from 'should';
 import steem from '../src';
+import { doesNotReject } from 'assert';
 
 const username = process.env.STEEM_USERNAME;
 const password = process.env.STEEM_PASSWORD;
@@ -8,16 +9,11 @@ const activeWif = password
   ? steem.auth.toWif(username, password, 'active')
   : '5HrfnP3V9wMP5P5uLqW1Ruj4sHFjGBJtt1moGJUzDqL5nvXTSyf';
 
-console.log(`------ activeWif: ${activeWif}`);
-
 describe('steem.broadcast:', () => {
   describe('setOptions', () => {
     it('works', () => {
       let url = steem.config.get('uri');
       if (!url) url = steem.config.get('websocket');
-      // steem.api.setOptions({ url: url, useAppbaseApi: true });
-      // steem.config.setOptions({ 'chain_id': '3ea6e60873c8ad342564b9f4b24def337be8c44509ce81a70d49c9169075dfcf' });
-      // steem.config.setOptions({ 'address_prefix': 'KWR' });
     });
   });
 
@@ -61,8 +57,6 @@ describe('steem.broadcast:', () => {
             ['account_expertise_update', payload]
           ],
         }, [activeWif]);
-
-        console.log('---------- account_expertise_update tx:', tx);
 
         // tx.should.have.properties([
         //   'expiration',
